8b61a24502c0b2f47aa9f2651193ee89f179cdbd
El Hadj Mbengue Arroyo <109061270+arroyostack@users.noreply.github.com>
2023-06-14 16:05:05 +0000

    essesment finished

--- src/app.js
+++ src/app.js
@@ -7,5 +7,21 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  const myList = document.getElementById("domain");
+  const combinations = (...args) => {
+    return args.reduce((a, b) => {
+      return a.map(x => b.map(y => [...x, y])).flat();
+    });
+  };
+
+  const output = combinations(
+    ["the", "our"],
+    ["great", "big"],
+    ["jogger", "racoon"]
+  ).map(item => item.join(""));
+
+  output.forEach(item => {
+    myList.innerHTML += `<li class="list-group-item list-group-item-action list-group-item-secondary">${item}.com</li>`;
+  });
+  console.log(myList);
 };
