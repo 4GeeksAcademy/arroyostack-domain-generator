6f493ee05d9ca738be8d00a70d55d8d6446997ac
El Hadj Mbengue Arroyo <109061270+arroyostack@users.noreply.github.com>
2023-06-19 18:25:20 +0000

    Legibility and formating improved

--- src/app.js
+++ src/app.js
@@ -1,27 +1,30 @@
-/* eslint-disable */
 import "bootstrap";
 import "./style.css";
-
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  const myList = document.getElementById("domain");
-  const combinations = (...args) => {
+  // Generates domains based on input
+  const generateDomains = (...args) => {
     return args.reduce((a, b) => {
       return a.map(x => b.map(y => [...x, y])).flat();
     });
   };
-
-  const output = combinations(
+  // Target Html element
+  const domainList = generateDomains(
     ["the", "our"],
     ["great", "big"],
     ["jogger", "racoon"]
-  ).map(item => item.join(""));
+  );
 
-  output.forEach(item => {
-    myList.innerHTML += `<li class="list-group-item list-group-item-action list-group-item-secondary">${item}.com</li>`;
+  // Join each single domains
+  const joinedDomains = domainList.map(domain => domain.join(""));
+
+  // Update content of target input
+  const htmlOutput = document.getElementById("domain");
+  joinedDomains.forEach(item => {
+    htmlOutput.innerHTML += `<li class="list-group-item list-group-item-action list-group-item-secondary">${item}.com</li>`;
   });
-  console.log(myList);
 };
+
+// Made with love by El-hadj
